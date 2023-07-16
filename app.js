8c9135d193738fe166f6dd234049c38083bb9449
Mhlevert <mhlevert@live.com>
2023-07-16 12:07:08 -0400

    last commit

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-// API à utiliser pour le travail: https://randomuser.me/api/?results=5
+
+const url = 'https://randomuser.me/api/?results=5';
+const fetchEmployes = async () => {
+    const response = await fetchEmployes(url);
+    const data = await response.json();
+    return data;
+}
+
+
+const container = document.querySelector('#container');
+
+const display = (users) => {
+    console.log(users);
+ 
+    const listeUsers = users.map((person) => {
+        const { picture, title, email} = person;
+        return `
+        
+            <img>${picture}</img>
+            <h5>${title}</h5>
+            <h6>${email}</h6>
+        `
+    })
+    container.innerHTML = listeUsers.join('');
+    
+}
+
+let index = 0;
+let pages = [];
+
+const init = async() => {
+    const followers = await fetchEmployes();
+
+    setupUI();
+}
+
+const setupUI = () => {
+    display(pages[index]);
+}
+
+init();
